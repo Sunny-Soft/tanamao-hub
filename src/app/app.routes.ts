02ecac8a707b4ed41d4fe8ff1c9c13fb8afbdf22
Cesar Gabriel <71941970+CesarGabriel26@users.noreply.github.com>
2026-03-27 08:49:47 -0300

    fix: pasta pages -> modules

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,14 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { DownloadComponent } from './pages/download/download.component';
-import { ConfigComponent } from './pages/config/config.component';
-import { LogsComponent } from './pages/logs/logs.component';
+import { HomeComponent } from './modules/home/home.component';
+import { DownloadComponent } from './modules/download/download.component';
+import { ConfigComponent } from './modules/config/config.component';
+import { LogsComponent } from './modules/logs/logs.component';
+import { ServicesComponent } from './modules/services/services.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'services', loadComponent: () => import('./pages/services/services.component').then(m => m.ServicesComponent) },
+  { path: 'services', component: ServicesComponent },
   { path: 'download', component: DownloadComponent },
   { path: 'config', component: ConfigComponent},
   { path: 'logs', component: LogsComponent },
